ad34d377a32f9b9ebdde79acb1a52a4189e6cff3
Ajdin Mehmedović <ayden.diagrampixel@gmail.com>
2025-04-12 14:33:49 +0200

    Randomizer

--- src/app/components/ImageCard.tsx
+++ src/app/components/ImageCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface ImageCardProps {
   image: {
@@ -12,19 +12,34 @@ interface ImageCardProps {
 const ImageCard: React.FC<ImageCardProps> = ({ image, detections }) => {
   return (
     <div className="bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer transform transition duration-200 hover:scale-105">
-      <img src={image.imageData} alt={`Image ${image.id}`} className="w-full h-56 object-cover" />
+      <img
+        src={image.imageData}
+        alt={`Image ${image.id}`}
+        className="w-full h-56 object-cover"
+      />
       <div className="p-4">
         <p className="text-lg font-semibold text-gray-800">
-          Captured at: {image.capturedAt ? new Date(image.capturedAt).toLocaleString() : 'Unknown'}
+          Captured at:{" "}
+          {image.capturedAt
+            ? new Date(image.capturedAt).toLocaleString()
+            : "Unknown"}
         </p>
+        <p className="text-xl font-bold">
+          Randomized value for image: {Math.floor(Math.random() * 1000)}
+        </p>
+
         {/* Show detection results */}
         {detections && detections.length > 0 && (
           <div>
-            <h3 className="text-xl font-semibold text-gray-800 mt-4">Detected Objects</h3>
+            <h3 className="text-xl font-semibold text-gray-800 mt-4">
+              Detected Objects
+            </h3>
             {detections.map((detection, idx) => (
               <div key={idx} className="text-gray-600 mt-2">
                 <p className="font-semibold">{detection.class}:</p>
-                <p className="text-sm">Confidence: {Math.round(detection.score * 100)}%</p>
+                <p className="text-sm">
+                  Confidence: {Math.round(detection.score * 100)}%
+                </p>
               </div>
             ))}
           </div>
